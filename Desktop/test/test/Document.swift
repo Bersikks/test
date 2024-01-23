6fb036f3709178cc52f61abd375ba58a029cf0f5
Berezhnova 3 <berezhnovaks@iMac6--Administrator.local>
2024-01-23 16:03:52 +0900

    test

---
+++ Desktop/test/test/Document.swift
@@ -0,0 +1,21 @@
+//
+//  Document.swift
+//  test
+//
+//  Created by Berezhnova <3 on 23.01.2024.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
